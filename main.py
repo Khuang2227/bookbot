cbc253a9e8a4fc9598b058d63db715130decbe20
Kevin Huang <khuang2227@gmail.com>
2025-10-06 14:17:19 -0400

    first code

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("greetings boots")
+def get_book_text():
+    with open("books/frankenstein.txt") as file:
+        text = file.read()
+        return text
+
+def main():
+    return get_book_text()
